30429f32cfe2fe14c1b5ffac7fd4d1aa1c7c5cc6
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-22 14:32:55 -0600

    updates flight routes redordering

--- routes/flights.js
+++ routes/flights.js
@@ -10,17 +10,27 @@ router.get('/new', flightsCtrl.new)
 // GET localhost:3000/flights/:id
 router.get('/:id', flightsCtrl.show)
 
-// POST localhost:3000/flights
-router.post('/', flightsCtrl.create)
 
 // POST localhost:3000/flights
-router.post('/:id/tickets', flightsCtrl.createTicket)
+router.post('/', flightsCtrl.create)
 
 // DELETE localhost:3000/flights/:id
 router.delete('/:id', flightsCtrl.delete)
 
-// DELETE localhost:3000/flights/:id
+
+// POST localhost:3000/flights/:id/tickets
+router.post('/:id/tickets', flightsCtrl.createTicket)
+
+// DELETE localhost:3000/flights/:id/tickets/:idx
 router.delete('/:id/tickets/:idx', flightsCtrl.deleteTicket)
+
+
+// POST localhost:3000/flights/:id/meals
+router.post('/:id/meals', flightsCtrl.addToMeal)
+
+// DELETE localhost:3000/flights/:id/meals/:id
+router.delete('/:id/meals/:mealId', flightsCtrl.removeMeal)
+
 
 export {
   router
